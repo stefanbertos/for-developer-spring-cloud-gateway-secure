8cd62af2c48351b16b81081158a910eb7cd3cb65
Stefan <stefan.bertos@gmail.com>
2023-04-03 14:34:05 +0200

    fix

--- README.md
+++ README.md
@@ -1,16 +1,28 @@
 # for-developer-spring-cloud-gateway-secure
 
-see https://www.baeldung.com/spring-boot-keycloak
-
 cd keycloak-20.0.3
 cd bin
 kc.bat start-dev
 
 admin/password
 
-https://medium.com/devops-dudes/securing-spring-boot-rest-apis-with-keycloak-1d760b2004e
+1.create new user admin/password
+2.login to the admin console
+3.create realm SpringBootKeycloak
+4.create client spring-cloud-gateway-secure-service - Client authentication = ON, Standard Flow, Direct grant access
+5.Settings -> set valid redirect urls = http://localhost:9000
+6.Credentials-> copy credential secret and place into this application.yml
+7.Roles->create role user
+8.User -> create user test -> Credentials -> set password test
 
-https://docs.spring.io/spring-security/reference/5.6.0-RC1/servlet/oauth2/oauth2-client.html
+http://localhost:8080/realms/SpringBootKeycloak/.well-known/openid-configuration
+
+
+http://localhost:9000/api/v1/client
+
+jwt.io
+
+https://docs.spring.io/spring-security/reference/servlet/oauth2/client/index.html
 
 
 https://github.com/ThomasVitale/spring-cloud-gateway-resilience-security-observability
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,39 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.WebFilterExchange;
+import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@Slf4j
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http.authorizeExchange(exchange -> exchange.matchers(EndpointRequest.toAnyEndpoint()).permitAll().anyExchange().authenticated()).oauth2Login((oauth2Login) -> oauth2Login.authenticationSuccessHandler(new RedirectServerAuthenticationSuccessHandler() {
+            @Override
+            public Mono<Void> onAuthenticationSuccess(WebFilterExchange webFilterExchange, Authentication authentication) {
+                DefaultOidcUser oidcUser = (DefaultOidcUser) authentication.getPrincipal();
+                log.info("onAuthenticationSuccess {}", oidcUser.getEmail());
+                return super.onAuthenticationSuccess(webFilterExchange, authentication);
+            }
+        })).build();
+    }
+
 
 }
+
+//todo add new header based on the
